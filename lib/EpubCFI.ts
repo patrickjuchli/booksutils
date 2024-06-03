6f47c2746a7793ef4f5e71685d9099c99e5d9cbe
Patrick Juchli <patrickjuchli@gmail.com>
2024-06-03 21:42:06 +0200

    Improve comments

--- lib/EpubCFI.ts
+++ lib/EpubCFI.ts
@@ -11,22 +11,23 @@ const PADDING_SIZE = 6
 const REGEX_SQUARE_BRACKET_EXPR = /\[.*\]!?/g
 
 /**
- * Converts CFI start location to comparable string that can be used for sorting. You can either provide `epubcfi(loc)` or
- * `epubcfi(parent,start,end)`. The result can only be used for fast comparisons, it's not meant to convey any other information.
+ * Converts a CFI start location to a string that can be used to quickly compare/sort locations. Accepts either `epubcfi(location)` or
+ * a range with `epubcfi(parent,start,end)`.
  * 
- * For example, from `epubcfi(/6/24[ch011_xhtml]!/4/320/3,:571,:651)` you'll get `000060002400004003200000300571`, a string
- * that you can use to compare locations in a sorting function.
+ * For example, from `epubcfi(/6/24[ch011_xhtml]!/4/320/3,:571,:651)` you'll get something like `000006000024000004000320000003000571`. 
+ * Treat this as an opaque value, use it only for comparing it to other locations, it's not meant to be parsed for other use.
  */
 export function cfiGetSortableStartLocation(cfi: string): string {
-    const numerical = cfi.replaceAll(REGEX_SQUARE_BRACKET_EXPR, "")
+    const numerical = cfi.replaceAll(REGEX_SQUARE_BRACKET_EXPR, "") // Strip tags
     const parts = numerical.split(",") // See https://idpf.org/epub/linking/cfi/#sec-ranges
     const parentAndStart = parts[0] + (parts[1] ?? "") 
     const matches = parentAndStart.match(/\d+/g)
     if (matches === null) {
         return ""
     }
-    // Padding each value in a CFI range start location will make the resulting strings comparable/sortable.
-    // It's assumed that a padding of PADDING_SIZE "oughta be enough for everyone". This assumption might break.
+    // Padding each value in a CFI range start location will make the resulting strings comparable/sortable even if 
+    // locations have varying numbers of elements. It's assumed that a padding of PADDING_SIZE "oughta be enough for everyone". 
+    // This assumption might break.
     return matches.map(m => m.padStart(PADDING_SIZE, "0")).join("")
 }
 
